98e47b18166fee5d6fbf064af037397dec8e7576
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-12-22 06:36:18 -0800

    Fixed werkzeug error and updated the puppet that installs flask from pip3.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,6 @@
 # It installs flask from pip3
 
-package { 'werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3',
-}
-
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['werkzeug'],
 }
