d0ef92213b5912412658471eefd6172168b02716
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-12-22 05:39:14 -0800

    Updated the puppet that installs flask from pip3.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,14 @@
 # It installs flask from pip3
 
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['werkzeug'],
 }
+
