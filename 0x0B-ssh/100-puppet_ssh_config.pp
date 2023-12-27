78533e40479fb2797ee88c569dd1c23ca3aba0f4
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-12-27 08:38:54 -0800

    Added a puppet that sets up your client SSH configuration file so that you can connect to a server without typing a password.

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,7 +2,6 @@ include stdlib
 # It sets up the client's SSH configuration file so that you can
 # connect to a server without typing a password.
 
-
 file_line{ 'SSH configuration':
   path => '/etc/ssh/ssh_config',
   line => "Host *\n\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no\n"
