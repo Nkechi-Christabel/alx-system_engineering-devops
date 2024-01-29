dc802659536a57922faa8719f2fac62993306ec2
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-29 10:27:28 -0800

    Wrote a script that fetches Rest ApI, returns a user's todos, name and export the data as Csv.

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Using the provided REST API and the given ID, returns information about
+his/her TODO list progress and export it in csv.
+"""
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    user_id = int(argv[1])
+    baseUrl = "https://jsonplaceholder.typicode.com/"
+    todo_res = requests.get(f'{baseUrl}todos')
+    user_res = requests.get(f"{baseUrl}/users/{user_id}")
+
+    if todo_res.status_code == 200 and user_res.status_code == 200:
+        todos, username = todo_res.json(), user_res.json().get('username')
+
+    with open(f"{user_id}.csv", 'w', newline='') as csvfile:
+        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS',
+                      'TASK_TITLE']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for task in todos:
+            writer.writerow({
+                'USER_ID': user_id,
+                'USERNAME': username,
+                'TASK_COMPLETED_STATUS': task['completed'],
+                'TASK_TITLE': task['title']
+            })
