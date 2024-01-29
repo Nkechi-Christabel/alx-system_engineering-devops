e0b50a1b01251ce2bed11e0f43e4cb12ed0d90d5
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-29 11:41:50 -0800

    Wrote a script that fetches Rest ApI, returns a user's todos, name and export the data as Csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -17,16 +17,8 @@ if __name__ == "__main__":
     if todos_res.status_code == 200 and user_res.status_code == 200:
         todos, user = todos_res.json(), user_res.json()
 
-    with open(f"{user_id}.csv", 'w', newline="") as csvfile:
-        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS',
-                      'TASK_TITLE']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
-                                quoting=csv.QUOTE_ALL)
-
+    with open(f'{user_id}.csv', 'w') as file:
         for task in todos:
-            writer.writerow({
-                'USER_ID': user_id,
-                'USERNAME': user.get('username'),
-                'TASK_COMPLETED_STATUS': task['completed'],
-                'TASK_TITLE': task['title']
-            })
+            file.write('"{}","{}","{}","{}"\n'
+                       .format(user_id, user['username'], task['completed'],
+                               task['title']))
