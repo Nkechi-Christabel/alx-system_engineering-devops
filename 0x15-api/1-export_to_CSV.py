3e37f6a27a8f7821c5471c66a6b1ba20c0d6d713
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-29 13:01:17 -0800

    Wrote a script that fetches Rest ApI, returns a user's todos, name and export the data as Csv.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,22 +11,23 @@ from sys import argv
 if __name__ == "__main__":
     user_id = int(argv[1])
     baseUrl = "https://jsonplaceholder.typicode.com/"
-    todos_res = requests.get(f"{baseUrl}users/{user_id}/todos")
+    todos_res = requests.get(f"{baseUrl}todos")
     user_res = requests.get(f"{baseUrl}users/{user_id}")
 
     if todos_res.status_code == 200 and user_res.status_code == 200:
         todos, user = todos_res.json(), user_res.json()
+        todos = [task for task in todos if task['userId'] == user_id]
 
-        with open(f"{user_id}.csv", 'w', newline='') as csvfile:
-            fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS',
+    with open(f"{user_id}.csv", 'w', newline='') as csvfile:
+        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS',
                       'TASK_TITLE']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
                                 quoting=csv.QUOTE_ALL)
 
-            for task in todos:
-                writer.writerow({
-                    'USER_ID': user_id,
-                    'USERNAME': user.get('username'),
-                    'TASK_COMPLETED_STATUS': task.get('completed'),
-                    'TASK_TITLE': task.get('title')
+        for task in todos:
+            writer.writerow({
+                'USER_ID': user_id,
+                'USERNAME': user.get('username'),
+                'TASK_COMPLETED_STATUS': task.get('completed'),
+                'TASK_TITLE': task.get('title')
             })
