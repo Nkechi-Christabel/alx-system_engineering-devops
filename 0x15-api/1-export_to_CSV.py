a5a2b90f543fa9a9d5e78e03aab7a476edc300ea
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-29 13:59:18 -0800

    Wrote a script that fetches Rest ApI, returns a user's todos, name and export the data as Csv.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
-"""Accessing a REST API for todo lists of employees"""
+"""
+Using the provided REST API and the given ID, returns information about
+his/her TODO list progress and export it in csv.
+"""
+import csv
 import requests
-import sys
+from sys import argv
 
 
-if __name__ == '__main__':
-    employeeId = sys.argv[1]
-    baseUrl = "https://jsonplaceholder.typicode.com/users"
-    url = baseUrl + "/" + employeeId
+if __name__ == "__main__":
+    user_id = int(argv[1])
+    baseUrl = "https://jsonplaceholder.typicode.com/"
+    todos_res = requests.get(f"{baseUrl}users/user_id/todos")
+    user_res = requests.get(f"{baseUrl}users/{user_id}")
 
-    response = requests.get(url)
-    username = response.json().get('username')
+    if todos_res.status_code == 200 and user_res.status_code == 200:
+        todos, user = todos_res.json(), user_res.json()
 
-    todoUrl = url + "/todos"
-    response = requests.get(todoUrl)
-    tasks = response.json()
+    with open(f"{user_id}.csv", 'w', newline='') as csvfile:
+        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS',
+                      'TASK_TITLE']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
+                                quoting=csv.QUOTE_ALL)
 
-    with open('{}.csv'.format(employeeId), 'w') as file:
-        for task in tasks:
-            file.write('"{}","{}","{}","{}"\n'
-                       .format(employeeId, username, task.get('completed'),
-                               task.get('title')))
+        for task in todos:
+            writer.writerow({
+                'USER_ID': user_id,
+                'USERNAME': user.get('username'),
+                'TASK_COMPLETED_STATUS': task.get('completed'),
+                'TASK_TITLE': task.get('title')
+            })
