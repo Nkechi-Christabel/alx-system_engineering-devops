1fd3e21e2487307b71f1c93e3b51f3987909ee24
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-02-13 04:29:15 -0800

    Wrote a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,6 +14,6 @@ def number_of_subscribers(subreddit):
 
     if response.status_code == 200:
         data = response.json()
-        return data['data']['subscribers']
+        return data.get('data').get('subscribers')
     else:
         return 0
