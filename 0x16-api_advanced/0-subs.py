9c7ae0e0493824a8d0b7f885f72e866dbe2911c9
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-02-13 04:59:30 -0800

    Wrote a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,32 +1,17 @@
 #!/usr/bin/python3
-"""
-A module to retrieve the number of subscribers for a given subreddit using
-the Reddit API.
-"""
+"""queries the Reddit API"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Retrieves the number of subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: The number of subscribers of the subreddit. Returns 0 if the
-             subreddit is invalid or not found.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User Agent'}
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        return data.get('data').get('subscribers')
+    """returns total subs"""
+    header = {"User-Agent": "Holberton"}
+    url = "https://www.reddit.com/r/" + subreddit + "/about.json"
+    r = requests.get(url, headers=header)
+    if r.status_code == 200:
+        return r.json().get("data", None).get("subscribers", None)
     else:
         return 0
-
 
 if __name__ == "__main__":
     pass
