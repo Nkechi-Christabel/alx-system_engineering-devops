f38c03ea2a982f24a48cf0805e5ac84a9b85f30f
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-02-14 11:33:44 -0800

    Fixed Apache 500 error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,15 @@
 # Fixes the reason for Apache returning 500 error
-exec { 'fix typo':
-   onlyif  => 'test -e /var/www/html/wp-settings.php',
-   command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+
+$wpfiles = [ '/var/www/html/.maintenance',
+          '/var/www/html/wp-content/languages',
+          '/var/www/html/wp-content/db.php',
+          '/var/www/html/wp-content/object-cache.php']
+
+file { $wpfiles:
+  ensure => 'present',
+}
+
+# copy wp needed from source
+file { '/var/www/html/wp-includes/class-wp-locale.phpp':
+  source => '/var/www/html/wp-includes/class-wp-locale.php'
 }
